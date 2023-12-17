43e350334b1a54260641e8a0b96b3e4e7ef5724d
Ronan Soares <ronanp2@hotmail.com>
2023-12-16 21:13:41 -0300

    User delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -30,4 +30,9 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);         // Para salvar o usuário no BD
 	}
+	
+	// Operação para deletar um usuário
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
